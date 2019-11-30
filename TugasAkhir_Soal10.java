503f2bd66a078aa72e2832673cb61cb17d486972
Andi Alifsyah <andyalyfsyah4@gmail.com>
2019-11-30 19:11:39 +0700

    Updated Works 1

--- TugasAkhir_Soal10.java
+++ TugasAkhir_Soal10.java
@@ -1,12 +1,17 @@
 import java.util.Scanner;
 
-public class TugasAkhir_Soal0 {
+public class TugasAkhir_Soal10 {
+    static String[] nama = {"Adi","Budi","Didin","Dodo","Candra","Lita","Nina","Citra","Tere"};
+    static String[] alamat = {"Jl. Sumbersari","Jl. Kertokertoan","Jl. Jalan","Jl.","Sigura","Jl. Veteran","Griya shanta","Griya brawijaya","Rusunawa","Dieng"};
+    static String[] noTelp = {"081234568","088721381","087293819","081237821","098213458","081234111","089999222","086523246"};
+    static String[] oper = {"XL","Indosat","Simpati","Axis","3","Simpati","XL","Indosat","3"};
+    static String[] grup = {"Pemdas","Pemlan","Pemdas" ,"ASD","Pemlan" ,"ASD","Pemdas","Jarkom ","Pemdas"};
 
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int pil = 0;
-        while (pil!=0){
+        while (pil!=7){
             System.out.println("1. Menambah Kontak baru");
             System.out.println("2. Mencetak Kontak");
             System.out.println("3. Mencari Kontak Berdasarkan Nama");
@@ -15,9 +20,13 @@ public class TugasAkhir_Soal0 {
             System.out.println("6. Menghapus Kontak Pada Daftar");
             System.out.println("7. Keluar");
             System.out.print("Masukkan pilihan anda : ");
-            int pil = in.nextInt();
+            pil = in.nextInt();
+            case
+            System.out.println();
 
+
+            System.out.println();
         };
-        System.out.println("PROGRAM TELAH SELESAI");
+          System.out.println(alamat[0]);
     }
 }
