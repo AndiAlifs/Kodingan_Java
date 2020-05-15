2acc6593f8491092ae430de45638f658434d2d3a
Andi Alifsyach <andyalyfsyah4@gmail.com>
2020-05-16 01:38:56 +0800

    Update konsepOOP1_1.java

--- pemlan/konsepOOP1_1.java
+++ pemlan/konsepOOP1_1.java
@@ -1,22 +1,18 @@
-class mobil{
-    public String warna;
-    public int kecepatan;
-    public void cetakAttribut(){
-        System.out.printf("%-10s: %s%n","Warna",warna);
-        System.out.printf("%-10s: %s","kecepatan",cekKecepatan());
+class Base {
+    public void show() {
+       System.out.println("Base::show() called");
     }
-    int cekKecepatan(){
-        return kecepatan;
+}
+
+class Derived extends Base {
+    public void show() {
+       System.out.println("Derived::show() called");
     }
-} 
+}
 
 public class konsepOOP1_1{
     public static void main(String[] args) {
-        i = 'a'
-        mobil x_pander = new mobil();
-        x_pander.kecepatan = 20;
-        x_pander.warna = "Merah";
-        x_pander.cetakAttribut();
+        Base b = new Derived();;
+        b.show();
     }
-    
 }
